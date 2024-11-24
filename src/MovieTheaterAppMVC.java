beafba1334286e28e0110b2297a1b7f16feb6a84
Shaun Sebastian <shaun.sebastian1@ucalgary.ca>
2024-11-24 07:12:08 -0700

    update

--- src/MovieTheaterAppMVC.java
+++
@@ -1,7 +0,0 @@
-public class MovieTheaterAppMVC {
-    public static void main(String[] args) {
-        DatabaseManager dbManager = new DatabaseManager();
-        MainView mainView = new MainView();
-        new MainController(dbManager, mainView);
-    }
-}
